af295ed1e73f762b1bb2281eb0eb225b6776129b
Alexander Niersmann <aniersmann@gmail.com>
2023-04-23 03:37:40 +0200

    Aufgabe InjectionToken und Factory

--- src/app/core/logger.service.ts
+++ src/app/core/logger.service.ts
@@ -1,10 +1,12 @@
-import { Injectable } from '@angular/core';
+import { Inject, Injectable } from '@angular/core';
+import { API_URL } from "./api-url";
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoggerService {
 
-  constructor() {
+  constructor(@Inject(API_URL) private _apiUrl: string) {
+    console.log(this._apiUrl);
   }
 }
