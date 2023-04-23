2146bf463749909a8a11ab24d06a71ac70c959ca
Alexander Niersmann <aniersmann@gmail.com>
2023-04-23 03:37:40 +0200

    Aufgabe InjectionToken und Factory

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { UserModule } from "./user/user.module";
 import { CoreModule } from "./core/core.module";
+import { API_URL } from "./core/api-url";
+import { Config, CONFIG } from "./core/config";
 
 @NgModule({
   declarations: [
@@ -14,7 +16,20 @@ import { CoreModule } from "./core/core.module";
     CoreModule,
     UserModule
   ],
-  providers: [],
+  providers: [
+    {provide: CONFIG, useValue: {isProd: true}},
+    {
+      provide: API_URL, useFactory: (config: Config) => {
+        if (config.isProd) {
+          return 'https://example.com';
+        }
+
+        return 'https://localhost:4200'
+      },
+      deps: [CONFIG]
+    }
+  ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
